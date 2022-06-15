325dff28faca676da5868559046200b7ac0b2752
gugledas <azabzistany@gmail.com>
2022-06-15 19:01:06 +0100

    maj hotlock

--- scss/organisme/sections/theme_builder/hotlock/js/header.js
+++ scss/organisme/sections/theme_builder/hotlock/js/header.js
@@ -1,46 +1,17 @@
-import Swiper, {Navigation, Pagination, Parallax, Autoplay} from "swiper"
+import Swiper, { Navigation, Pagination, Parallax, Autoplay } from "swiper";
 Swiper.use([Navigation, Pagination, Parallax, Autoplay]);
 
-
 var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    slidesPerView: 1,
-    spaceBetween: 30,
-    mousewheel: false,
-    loop: true,
-    autoplay: {
-      Delay: 5000,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
-
-let bacTop = document.querySelector(".bac-top-btn");
-bacTop.addEventListener("click", scrollUp);
-
-window.onscroll = () => {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 500 ||
-    document.documentElement.scrollTop > 500
-  ) {
-    bacTop.style.visibility = "visible";
-    bacTop.style.opacity = "1";
-  } else {
-    bacTop.style = "";
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 0,
+  mousewheel: false,
+  loop: true,
+  autoplay: {
+    Delay: 5000
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
   }
-}
-
-function scrollUp() {
-  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: "smooth"
-  });
-}
+});
