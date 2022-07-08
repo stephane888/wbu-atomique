a47e3dfc650443c88d1877f78a1e6ddd8c30e2cc
gugledas <azabzistany@gmail.com>
2022-07-08 13:53:22 +0100

    maj

---
+++ scss/organisme/sections/theme_builder/f_news/js/sticky-author.js
@@ -0,0 +1,54 @@
+(function () {
+  "use-strict";
+  /* chargement du js sdk */
+  (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+  /*  */
+  function initFacebookShare() {
+    if (window.FB) {
+      FB.init({
+        appId: "327735579426150",
+        xfbml: true,
+        version: "v3.0"
+      });
+      console.log("sdk init");
+    } else {
+      setTimeout(initFacebookShare, 1000);
+      console.log("Le sdk js n'est pas chargé");
+    }
+  }
+  /*  */
+  function openShare() {
+    let host = window.location.origin + window.location.pathname;
+    FB.ui(
+      {
+        method: "share",
+        href: host
+      },
+      function (response) {
+        console.log("reponse", response);
+      }
+    );
+  }
+  window.addEventListener("load", () => {
+    var facebookButton = document.querySelector(".facebookShare");
+    if (facebookButton) {
+      initFacebookShare();
+      facebookButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        openShare();
+      });
+    } else {
+      console.log(
+        'le bouton de partage facebook avec la classe ("facebookShare") est absent sur cette page'
+      );
+    }
+  });
+})();
