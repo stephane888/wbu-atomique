555dd47a4d4f17d43747a503725b2eefd418c43e
nzouda777 <rodriguenzouda@gmail.com>
2022-08-23 11:06:14 +0100

    hero slider

--- scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js
+++ scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js
@@ -1,3 +1,23 @@
 
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
+import 'jquery'
+$(document).ready(function(){
+    $('.slider-handler').owlCarousel({
+        loop: true,
+        margin: 5,
+        nav: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000:{
+                items: 2
+            }
+        }
+    })
+})     
