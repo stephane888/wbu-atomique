cf2f398c8d1466f7823bf22b652b56b34bd85fcf
gugledas <azabzistany@gmail.com>
2022-08-24 14:10:33 +0100

    maj caroussel

--- scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js
+++ scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js
@@ -1,4 +1,4 @@
-import Swiper, { Navigation, Pagination } from "swiper";
+import Swiper, { Controller, Navigation, Pagination, Thumbs } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 
@@ -11,4 +11,26 @@ const swiper_un = new Swiper(".carousel-hero", {
   direction: "horizontal",
   loop: true
 });
+
+const thumbs = new Swiper(".thumbnail-swiper", {
+  modules: [Controller],
+  loop: true,
+  spaceBetween: 5,
+  slidesPerView: 3,
+  slidesPerScroll: 5,
+  slideToClickedSlide: true
+});
+var thum_swiper = new Swiper(".thumbs-carousel", {
+  modules: [Pagination, Controller, Thumbs],
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  thumbs: {
+    swiper: thumbs,
+    autoScrollOffset: 1
+  }
+});
+
 console.log("swiper on", swiper_un);
