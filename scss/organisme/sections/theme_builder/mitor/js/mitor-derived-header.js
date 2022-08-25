8dd3c2090ed28cd66a1587d5bd5ea490f99250ef
gugledas <azabzistany@gmail.com>
2022-08-25 13:29:34 +0100

    maj carousel testimony

--- scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js
+++ scss/organisme/sections/theme_builder/mitor/js/mitor-derived-header.js
@@ -12,6 +12,43 @@ const swiper_un = new Swiper(".carousel-hero", {
   loop: true
 });
 
+const carousel_testy = new Swiper(".carousel-testy", {
+  modules: [Pagination],
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  loopedSlides: 4,
+  spaceBetween: 20,
+  //slidesPerGroup: 2,
+  slidesPerView: 2,
+  loop: true
+});
+const project_tabs = new Swiper(".project-tabs", {
+  modules: [Pagination],
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  slideClass: "mpt-img",
+  slideActiveClass: "mpt-img--active",
+  loopedSlides: 4,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  loop: true,
+  breakpoints: {
+    769: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    1200: {
+      slidesPerView: 3
+    }
+  }
+});
+
 const thumbs = new Swiper(".thumbnail-swiper", {
   modules: [Controller],
   loop: true,
