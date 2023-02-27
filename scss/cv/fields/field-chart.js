2ee482e36b715f52f890d6ea89613da15a315530
kouwa stephane <kksasteph888@gmail.com>
2023-02-27 10:30:52 +0100

    Maj

---
+++ scss/cv/fields/field-chart.js
@@ -0,0 +1,4 @@
+// pour le moment on fait un import brute, ensuite on va organiser.
+//import Chart from "chart.js/auto";
+// pour une raison inconnue ce fichier ne s'execute pas ????
+console.log(" Chart SJ lllkokkik582 ");
