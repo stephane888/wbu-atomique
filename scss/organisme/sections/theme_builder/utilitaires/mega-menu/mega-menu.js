2cd1f8a75929d5629fa0a23ba8cade58f185a3f3
kouwa stephane <kksasteph888@gmail.com>
2023-09-02 07:58:22 +0200

    build menu menu desctop

--- scss/organisme/sections/theme_builder/utilitaires/mega-menu/mega-menu.js
+++ scss/organisme/sections/theme_builder/utilitaires/mega-menu/mega-menu.js
@@ -4,15 +4,66 @@
 class HbkMegaMenu {
   constructor() {
     /**
-     * Ce selecteur doit est un enfant direct de la balise conteneur.
-     * il ajoute|retire la classe open
-     */
-    this.selectorOpenClose = document.querySelectorAll(".hbk-mega-menu .js-open-close");
-    /**
      * Contient tous les elements de menus.
      */
     this.items = [];
   }
+  /**
+   * Ajoute les icones svg, à tous les elements qui possedent de
+   * sous menus + un ecouter de click.
+   */
+  addIconSvgOnItem() {
+    const items = document.querySelectorAll(".hbk-mega-menu .item");
+    items.forEach((item) => {
+      const submenu = item.querySelector(".hbk-mega-menu--items__sub");
+      const MegaMenu = item.querySelector(".hbk-mega-menu--block");
+      if (submenu || MegaMenu) {
+        item.appendChild(this.generateIconSvg());
+        const svgElement = item.querySelector(".js-open-close");
+        this.addEventListenerToSvg(svgElement);
+      }
+    });
+  }
+
+  /**
+   * Permet d'afficher un sous menu au click.
+   */
+  toggleSubMenu() {
+    this.addIconSvgOnItem();
+  }
+
+  /**
+   *
+   * @param {*} svgElement( <svg> )
+   */
+  addEventListenerToSvg(svgElement) {
+    const container = document.querySelector(".hbk-mega-menu");
+    const element = svgElement.parentElement;
+    svgElement.addEventListener("click", (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      const OpenMenu = element.classList.contains("open");
+      this.CloseAllItems().then(() => {
+        if (OpenMenu) element.classList.remove("open");
+        else element.classList.add("open");
+        if (!OpenMenu) container.classList.add("has-open-item");
+        else container.classList.remove("has-open-item");
+      });
+    });
+  }
+
+  CloseAllItems() {
+    return new Promise((resolv) => {
+      const items = document.querySelectorAll(".hbk-mega-menu .item");
+      items.forEach((item) => {
+        item.classList.remove("open");
+      });
+      setTimeout(() => {
+        resolv(true);
+      }, 350);
+    });
+  }
+
   /**
    * Construit les itemps pour les menus classic.
    */
@@ -42,23 +93,28 @@ class HbkMegaMenu {
         }
       });
     }
-    console.log("items  : ", this.items);
   }
-
   /**
-   * Permet d'afficher un sous menu et de fermer le reste.
+   * --
+   * @returns
    */
-  toggleSubMenu() {
-    if (this.selectorOpenClose)
-      this.selectorOpenClose.forEach((item) => {
-        const element = item.parentElement;
-        if (element)
-          element.addEventListener("click", (event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            element.classList.toggle("open");
-          });
-      });
+  generateIconSvg() {
+    const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const iconPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    //
+    iconSvg.setAttribute("viewBox", "0 0 100 80");
+    iconSvg.setAttribute("height", "20px");
+    iconSvg.setAttribute("width", "20px");
+    iconSvg.setAttribute("xml:space", "preserve");
+    iconSvg.classList.add("svg-arrow");
+    iconSvg.classList.add("js-open-close");
+    //
+    iconPath.setAttribute(
+      "d",
+      "M 90 24.25 c 0 -0.896 -0.342 -1.792 -1.025 -2.475 c -1.366 -1.367 -3.583 -1.367 -4.949 0 L 45 60.8 L 5.975 21.775 c -1.367 -1.367 -3.583 -1.367 -4.95 0 c -1.366 1.367 -1.366 3.583 0 4.95 l 41.5 41.5 c 1.366 1.367 3.583 1.367 4.949 0 l 41.5 -41.5 C 89.658 26.042 90 25.146 90 24.25 z"
+    );
+    iconSvg.appendChild(iconPath);
+    return iconSvg;
   }
 }
 
