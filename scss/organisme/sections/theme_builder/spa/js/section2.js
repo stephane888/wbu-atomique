49585cc5748c43a857540c4b83d4cbc22b66a9ef
laurelle <obilamessomoangelelaurelle@gmail.com>
2024-02-27 16:09:26 +0000

    majj

---
+++ scss/organisme/sections/theme_builder/spa/js/section2.js
@@ -0,0 +1,17 @@
+import 'swiper'
+import Swiper from 'swiper'
+
+new Swiper(".swiper-full-option", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+  
