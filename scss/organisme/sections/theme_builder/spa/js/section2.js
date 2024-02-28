159a7937967eba862773a56f8cdb5686b3e6dc05
laurelle <obilamessomoangelelaurelle@gmail.com>
2024-02-28 08:47:45 +0000

    creation du slide

--- scss/organisme/sections/theme_builder/spa/js/section2.js
+++ scss/organisme/sections/theme_builder/spa/js/section2.js
@@ -1,17 +1,83 @@
-import 'swiper'
-import Swiper from 'swiper'
 
-new Swiper(".swiper-full-option", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-  
+
+/**
+ * Pour l'env. Drupal, il faudra importer : import "@stephane888/wbu-atomique/js/swiper/swiper-drupal.js";
+ */
+import SwiperManager from "../../../../../../js/swiper/swiper";
+const currentProd = {
+  direction: "horizontal",
+  effect: "slide",
+  speed: 2000,
+  spaceBetween: 10,
+  loop: true,
+  zoom: false,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+    enabled: 1,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    enabled: 1,
+  },
+  parallax: true,
+  autoplay: {
+    delay: 2,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+    waitForTransition: true,
+    stopOnLastSlide: false,
+  },
+  centeredSlides: 0,
+  freeMode: {
+    enabled: true,
+    minimumVelocity: 0.2,
+    momentum: true,
+    momentumBounce: true,
+    momentumBounceRatio: 1,
+    momentumRatio: 1,
+    momentumVelocityRatio: 1,
+    sticky: 1,
+  },
+  slidesPerView: "1",
+  grabCursor: 0,
+};
+/**
+ * TOP
+ */
+const settingsGalleryTops = {
+  direction: "horizontal",
+  effect: "coverflow",
+  speed: 10000,
+  spaceBetween: 10,
+  loop: true,
+  pagination: { el: ".swiper-pagination", type: "bullets", clickable: true, enabled: 1 },
+  navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev", enabled: 1 },
+  parallax: false, // https://swiperjs.com/swiper-api#parallax
+  freeMode: {
+    enabled: true,
+    minimumVelocity: 0.2,
+    momentum: true,
+    momentumBounce: true,
+    momentumBounceRatio: 1,
+    momentumRatio: 1,
+    momentumVelocityRatio: 1,
+    sticky: 1,
+  },
+  autoplay: {
+    delay: 2,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+    waitForTransition: true,
+    stopOnLastSlide: false,
+  },
+  centeredSlides: 0,
+  slidesPerView: 1,
+  grabCursor: 0,
+};
+document.querySelector(".testslider.swiper-full-options").setAttribute("data-swiper", JSON.stringify(settingsGalleryTops));
+
+const galleryThumbs = new SwiperManager();
+galleryThumbs.build();
