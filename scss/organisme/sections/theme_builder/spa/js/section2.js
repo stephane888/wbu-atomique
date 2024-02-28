f1dbd13ff581c21f0d582f3b2a93505e229c4fa0
laurelle <obilamessomoangelelaurelle@gmail.com>
2024-02-28 14:04:01 +0000

    modification du slide

--- scss/organisme/sections/theme_builder/spa/js/section2.js
+++ scss/organisme/sections/theme_builder/spa/js/section2.js
@@ -4,53 +4,12 @@
  * Pour l'env. Drupal, il faudra importer : import "@stephane888/wbu-atomique/js/swiper/swiper-drupal.js";
  */
 import SwiperManager from "../../../../../../js/swiper/swiper";
-const currentProd = {
-  direction: "horizontal",
-  effect: "slide",
-  speed: 2000,
-  spaceBetween: 10,
-  loop: true,
-  zoom: false,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: true,
-    enabled: 1,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-    enabled: 1,
-  },
-  parallax: true,
-  autoplay: {
-    delay: 2,
-    pauseOnMouseEnter: true,
-    disableOnInteraction: false,
-    waitForTransition: true,
-    stopOnLastSlide: false,
-  },
-  centeredSlides: 0,
-  freeMode: {
-    enabled: true,
-    minimumVelocity: 0.2,
-    momentum: true,
-    momentumBounce: true,
-    momentumBounceRatio: 1,
-    momentumRatio: 1,
-    momentumVelocityRatio: 1,
-    sticky: 1,
-  },
-  slidesPerView: "1",
-  grabCursor: 0,
-};
-/**
- * TOP
- */
+
+
 const settingsGalleryTops = {
   direction: "horizontal",
-  effect: "coverflow",
-  speed: 10000,
+  effect: "cover",
+  speed: 5000,
   spaceBetween: 10,
   loop: true,
   pagination: { el: ".swiper-pagination", type: "bullets", clickable: true, enabled: 1 },
