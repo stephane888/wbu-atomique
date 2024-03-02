539839c753f2f88721398652a2029acb317ea99b
kouwa stephane <kksasteph888@gmail.com>
2024-03-02 12:06:44 +0100

    update

--- js/bootstrap/all.js
+++ js/bootstrap/all.js
@@ -11,3 +11,7 @@ import "bootstrap/js/src/toast.js";
 import "bootstrap/js/src/tooltip.js";
 
 //import "bootstrap/js/dist/tooltip";
+
+// Bootstrap 5 propose egalement :
+// node_modules/bootstrap/js/index.umd.js
+// node_modules/bootstrap/js/index.esm.js
