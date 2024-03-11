bfc9c4b869281d4a5cc06cf42d22591ccc37e6ac
vystii <ngahabanda@gmail.com>
2024-03-11 15:33:41 +0100

    correction sur les fichiers de gallery overlay pour drupal

--- js/gallery-overlay/drupal-gallery-overlay.js
+++ js/gallery-overlay/drupal-gallery-overlay.js
@@ -3,11 +3,17 @@ import GalleryOverlay from "./gallery-overlay";
  * il faudra documenter.
  */
 (function (Drupal) {
-    Drupal.behaviors.swiperjs_options = {
+    Drupal.behaviors.gallery_overlay_options = {
         attach: function (context, settings) {
             if (context.querySelectorAll && context.querySelectorAll(".drupal-gallery-overlay").length) {
-                const dg = new GalleryOverlay(context);
-                dg.build();
+                console.log(...context.querySelector(".drupal-gallery-overlay").classList);
+                if (![...context.querySelector(".drupal-gallery-overlay").classList].includes("loaded")) {
+                    const dg = new GalleryOverlay(context);
+                    dg.build();
+                    [...context.querySelectorAll(".drupal-gallery-overlay")].forEach(element => {
+                        element.classList.add("loaded")
+                    });
+                }
             }
         },
     };
