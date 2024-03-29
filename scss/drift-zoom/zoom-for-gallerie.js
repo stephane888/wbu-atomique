aa37a0671af0040a7989577a2f9bda6457091427
kouwa stephane <kksasteph888@gmail.com>
2024-03-29 09:42:35 +0100

    update

--- scss/drift-zoom/zoom-for-gallerie.js
+++ scss/drift-zoom/zoom-for-gallerie.js
@@ -6,23 +6,37 @@ import Drift from "drift-zoom";
 import "./zoom-for-gallerie.scss";
 
 /**
- * il faudra documenter.
+ * Fonctionne avec le slider.
  */
 (function (Drupal) {
-  Drupal.behaviors.more_fields = {
+  Drupal.behaviors.moreFieldsBehavior = {
     attach: function (context, settings) {
-      if (context.querySelectorAll && context.querySelectorAll(".galleries-main img").length) {
-        const imgs = context.querySelectorAll(".galleries-main img");
-        const paneContainer = document.querySelector(".galleries-main-thumbs");
-        imgs.forEach((img) => {
-          new Drift(img, {
-            paneContainer: paneContainer,
+      const ManageDriftOnimage = (img, instanceDrif) => {
+        // img.classList.add("run-drift");
+        // instanceDrif.disable();
+        img.addEventListener("click", () => {
+          if (img.classList.toggle("run-drift")) {
+            //console.log("disable drift : ", img);
+            instanceDrif.disable();
+          } else {
+            //console.log("enable drift : ", img);
+            instanceDrif.enable();
+          }
+        });
+      };
+      once("GallerieMoreFieldsBehavior", ".galleries-main", context).forEach(function (GallerieContainer) {
+        once("imgMoreFieldsBehavior", ".swiper-slide img", GallerieContainer).forEach((img) => {
+          const drift = new Drift(img, {
+            paneContainer: GallerieContainer,
             inlinePane: false,
             handleTouch: false,
             zoomFactor: 2.0,
+            hoverDelay: 200,
+            touchDelay: 200,
           });
+          ManageDriftOnimage(img, drift);
         });
-      }
+      });
     },
   };
 })(window.Drupal);
