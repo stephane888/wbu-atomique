645ab4eac8152ac8b1abbe6743333f76ba9c2d4c
kouwa stephane <kksasteph888@gmail.com>
2024-12-13 13:59:59 +0100

    Maj

--- scss/drupal/modules/more_fields/field/more_fields_video_with_converter/more_fields_video_with_converter.js
+++ scss/drupal/modules/more_fields/field/more_fields_video_with_converter/more_fields_video_with_converter.js
@@ -2,17 +2,6 @@ Drupal.behaviors.more_fields_video_with_converter = {
   attach: function (context, settings) {
     // On se rassure que l'element est present dans le context.
     if (context.querySelectorAll && context.querySelectorAll(".video-container")) {
-      const configs = {
-        read_auto: true, // Lit la video de maniere automatique.
-        muted: false, // son desactivé par defaut
-        read_mouse_enter: true, //  Demarre la lecture si le cuiseur survol la video.
-        stop_video_over_display: true, // Stop la video si'elle n'est plus dans la zone visible (50%)
-        start_video_is_display: false,
-        show_custom_control: true,
-      };
-      // Ajustement de la config
-      if (configs.read_auto && configs.read_mouse_enter) configs.read_auto = 0;
-
       // On se rasure que l'element est lu 1 seule foix.
       /**
        * Permet de jourer la video.
@@ -64,90 +53,120 @@ Drupal.behaviors.more_fields_video_with_converter = {
       };
       once("more_fields_video_with_converter", ".video-container", context).forEach((item) => {
         const eltControls = item.querySelector(".elt-controls");
-        const play = item.querySelector(".play");
-        const pause = item.querySelector(".pause");
-        const volumeHigh = item.querySelector(".volume-high");
-        const volumeOff = item.querySelector(".volume-off");
-        const video = item.querySelector("video.videos_control");
-        eltControls.style.display = "flex";
-        console.log("configs : ", configs);
-        if (configs.show_custom_control) {
-          if (!configs.read_auto) {
-            pauseVideo(video, play, pause, "Event lister pause");
-          }
-          if (configs.read_auto && !video.paused) {
-            // Si le paramettre autoplay=1
-            // On se rassure que la video a effectivement demarrer.
-            playVideo(video, play, pause, "auto_read");
-            // Si on joue la video, on doit pouvoir controler le volume.
-            mutedVideo(video, volumeHigh, volumeOff);
-          }
-          // li la video au survol
-          if (configs.read_mouse_enter)
+        if (eltControls) {
+          const dataSettings = eltControls.getAttribute("data-settings") ? JSON.parse(eltControls.getAttribute("data-settings")) : {};
+          console.log("dataSettings : ", dataSettings);
+          const configs = {
+            read_auto: true, // Lit la video de maniere automatique.
+            muted: false, // son desactivé par defaut
+            read_mouse_enter: true, //  Demarre la lecture si le cuiseur survol la video.
+            stop_video_over_display: true, // Stop la video si'elle n'est plus dans la zone visible (50%)
+            start_video_is_display: false,
+            show_custom_control: true,
+            ...dataSettings,
+          };
+          // Ajustement de la config
+          if (configs.read_auto && configs.read_mouse_enter) configs.read_auto = 0;
+          const play = item.querySelector(".play");
+          const pause = item.querySelector(".pause");
+          const volumeHigh = item.querySelector(".volume-high");
+          const volumeOff = item.querySelector(".volume-off");
+          const video = item.querySelector("video.videos_control");
+          eltControls.style.display = "flex";
+          console.log("configs : ", configs);
+          if (configs.show_custom_control) {
+            if (!configs.read_auto) {
+              pauseVideo(video, play, pause, "Event lister pause");
+            }
+            if (configs.read_auto && !video.paused) {
+              // Si le paramettre autoplay=1
+              // On se rassure que la video a effectivement demarrer.
+              playVideo(video, play, pause, "auto_read");
+              // Si on joue la video, on doit pouvoir controler le volume.
+              mutedVideo(video, volumeHigh, volumeOff);
+            }
+            // li la video au survol
+            if (configs.read_mouse_enter)
+              video.addEventListener(
+                "mouseenter",
+                () => {
+                  playVideo(video, play, pause, "read_by_mouse_enter");
+                  // Si on joue la video, on doit pouvoir controler le volume.
+                  mutedVideo(video, volumeHigh, volumeOff);
+                },
+                false
+              );
+            // On determiner le bouton à afficher
             video.addEventListener(
-              "mouseenter",
+              "play",
               () => {
-                playVideo(video, play, pause, "read_by_mouse_enter");
+                playVideo(video, play, pause, "event lister play");
                 // Si on joue la video, on doit pouvoir controler le volume.
                 mutedVideo(video, volumeHigh, volumeOff);
               },
               false
             );
-          // On determiner le bouton à afficher
-          video.addEventListener(
-            "play",
-            () => {
-              playVideo(video, play, pause, "event lister play");
-              // Si on joue la video, on doit pouvoir controler le volume.
-              mutedVideo(video, volumeHigh, volumeOff);
-            },
-            false
-          );
-          video.addEventListener(
-            "pause",
-            () => {
-              pauseVideo(video, play, pause, "Event lister pause");
-            },
-            false
-          );
-          video.addEventListener(
-            "volumechange",
-            () => {
-              mutedVideo(video, volumeHigh, volumeOff);
-            },
-            false
-          );
-          //
-          pause.addEventListener(
-            "click",
-            () => {
-              video.pause();
-            },
-            false
-          );
-          play.addEventListener(
-            "click",
-            () => {
-              video.play();
-            },
-            false
-          );
-          volumeOff.addEventListener(
-            "click",
-            () => {
-              video.muted = false;
-              video.volume = 0.5;
-            },
-            false
-          );
-          volumeHigh.addEventListener(
-            "click",
-            () => {
-              video.muted = true;
-            },
-            false
-          );
-          /////
+            video.addEventListener(
+              "pause",
+              () => {
+                pauseVideo(video, play, pause, "Event lister pause");
+              },
+              false
+            );
+            video.addEventListener(
+              "volumechange",
+              () => {
+                mutedVideo(video, volumeHigh, volumeOff);
+              },
+              false
+            );
+            //
+            pause.addEventListener(
+              "click",
+              () => {
+                video.pause();
+              },
+              false
+            );
+            play.addEventListener(
+              "click",
+              () => {
+                video.play();
+              },
+              false
+            );
+            volumeOff.addEventListener(
+              "click",
+              () => {
+                video.muted = false;
+                video.volume = 0.5;
+              },
+              false
+            );
+            volumeHigh.addEventListener(
+              "click",
+              () => {
+                video.muted = true;
+              },
+              false
+            );
+            //
+            // Créez un observateur d'intersection
+            const observer = new IntersectionObserver(
+              (entries) => {
+                entries.forEach((entry) => {
+                  if (!entry.isIntersecting) {
+                    !video.pause();
+                  }
+                });
+              },
+              {
+                threshold: 0.5, // Ajustez ce seuil selon vos besoins
+              }
+            );
+            // Observez la vidéo
+            observer.observe(video);
+          }
         }
       });
     }
