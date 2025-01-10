a1a85a7dd34d0b80bdb34ccdf01d84ca506bcd3b
kouwa stephane <kksasteph888@gmail.com>
2025-01-10 04:16:04 +0100

    Maj

--- js/test/try_code.js
+++ js/test/try_code.js
@@ -3,3 +3,20 @@
  * on peut ajouter "import "@stephane888/wbu-atomique/js/test/try_code.js;"
  */
 // Code for orthers things.
+
+//
+// Drupal.behaviors.customScroll = {
+//     attach: function (context, settings) {
+//         if (document.querySelector('.custom-paragraph-scroll')) {
+//             const elements = once("not-front-page", '.custom-paragraph-scroll', context)
+//             console.log("elements = ", elements);
+//             window.once('not-front-page').on('scroll', function () {
+//                 if (window.pageYOffset > 50) { // Ajustez la valeur selon vos besoins
+//                 document.body.classList.add('scrolled');
+//                 } else {
+//                 document.body.classList.remove('scrolled');
+//                 }
+//                 });
+//         }
+//     }
+// }
