243b54ecf95695eb64776cb7def35203db2f5b65
kouwa stephane <kksasteph888@gmail.com>
2025-01-18 14:50:44 +0100

    Maj

--- js/gallery-overlay/gallery-overlay.js
+++ js/gallery-overlay/gallery-overlay.js
@@ -50,6 +50,12 @@ class GalleryOverlay {
      * contient l'element qui vient d'etre selectionner.
      */
     this.currentItem = null;
+    /**
+     * Largeur du layout. i.e espace utilisable.
+     */
+    this.layout = { width: context.offsetWidth, height: window.innerHeight };
+    //this.windowWidth = window.innerWidth;
+    //this.windowHeight = window.innerHeight;
   }
 
   /**
@@ -72,14 +78,16 @@ class GalleryOverlay {
     }
     this.listernerClick(imagesOverlay, generatedElement);
     // document.generatedElement = generatedElement;
-    generatedElement.overlay.prepend(generatedElement.prevButton);
+    // generatedElement.overlay.prepend(generatedElement.prevButton);
     generatedElement.overlay.appendChild(generatedElement.imagesContainer);
-    generatedElement.overlay.appendChild(generatedElement.nextButton);
+    // generatedElement.overlay.appendChild(generatedElement.nextButton);
     generatedElement.imagesContainer.appendChild(generatedElement.image);
     generatedElement.imagesContainer.appendChild(generatedElement.exitButton);
     generatedElement.imagesContainer.appendChild(generatedElement.loader);
+    generatedElement.imagesContainer.appendChild(generatedElement.prevButton);
+    generatedElement.imagesContainer.appendChild(generatedElement.nextButton);
     this.context.appendChild(generatedElement.overlay);
-    // On appeche la propagation du click sur l'element parent.
+    // On empeche la propagation du click sur l'element parent.
     generatedElement.imagesContainer.addEventListener("click", (event) => {
       event.stopPropagation();
     });
@@ -161,7 +169,7 @@ class GalleryOverlay {
         event.preventDefault();
         this.currentItem = event.target;
         this.OpenPopup(generatedElement);
-        this.LoadImage(generatedElement);
+        this.LoadImage(generatedElement, "click");
         // console.log(event.target.getAttribute("href"));
       });
     });
@@ -172,8 +180,9 @@ class GalleryOverlay {
    * @param {*} location
    * @returns
    */
-  LoadImage(generatedElement) {
+  LoadImage(generatedElement, source = "navigation") {
     return new Promise((resolv) => {
+      generatedElement.overlay.classList.add("loading-file");
       this.toggleLoader(generatedElement, true);
       const runTransition = { with: false, height: false };
       const loaderImage = () => {
@@ -181,11 +190,36 @@ class GalleryOverlay {
           const img = document.createElement("img");
           img.addEventListener("load", () => {
             setTimeout(() => {
+              let margin = 30;
+              var imgWidth = img.naturalWidth;
+              var imgHeight = img.naturalHeight;
+              const aspectRatio = img.naturalWidth / img.naturalHeight;
+              // Si la hauteur de l'image depasse la taille, de l'ecran, on fixe la hauteur et on determine la largeur.
+              if (img.naturalHeight + margin >= this.layout.height) {
+                const fixeHeight = this.layout.height - margin - 15;
+                imgHeight = fixeHeight;
+                imgWidth = fixeHeight * aspectRatio;
+                if (imgWidth > this.layout.width) {
+                  const fixeWidth = this.layout.width;
+                  imgWidth = fixeWidth;
+                  imgHeight = fixeWidth / aspectRatio;
+                }
+              } else if (img.naturalWidth > this.layout.width) {
+                const fixeWidth = this.layout.width;
+                imgWidth = fixeWidth;
+                imgHeight = fixeWidth / aspectRatio;
+              }
+              console.log("imgWidth/imgHeight : ", imgWidth + "/" + imgHeight);
+              console.log("layout.dimension  : ", this.layout);
               // On ajuste la taille du container par rapport à l'image reelle.
-              generatedElement.imagesContainer.style.width = img.naturalWidth + "px";
-              generatedElement.imagesContainer.style.height = img.naturalHeight + "px";
+              generatedElement.imagesContainer.style.width = imgWidth + "px";
+              generatedElement.imagesContainer.style.height = imgHeight + "px";
               generatedElement.image.setAttribute("src", img.src);
               generatedElement.image.classList.add("show");
+              //
+              // generatedElement.image.style.width = imgWidth + "px";
+              // generatedElement.image.style.height = imgHeight + "px";
+              //
               this.toggleLoader(generatedElement, false);
               img_resolv(img);
             }, 100);
@@ -201,15 +235,23 @@ class GalleryOverlay {
           if (!runTransition.with && event.propertyName == "width") {
             runTransition.with = true;
             // console.log("La transition est terminée.", event.propertyName);
-            generatedElement.imagesContainer.style.width = generatedElement.image.offsetWidth + "px";
+            // generatedElement.imagesContainer.style.width = generatedElement.image.offsetWidth + "px";
+            generatedElement.overlay.classList.remove("loading-file");
           }
           // On verifie si la transition sur width est terminée.
           if (!runTransition.height && event.propertyName == "height") {
             runTransition.height = true;
             // console.log("La transition est terminée.", event.propertyName);
-            generatedElement.imagesContainer.style.height = generatedElement.image.offsetHeight + "px";
+            // generatedElement.imagesContainer.style.height = generatedElement.image.offsetHeight + "px";
+            generatedElement.overlay.classList.remove("loading-file");
           }
         });
+        //
+        if (source == "click") generatedElement.overlay.classList.remove("loading-file");
+        // Dans la mesure ou on a pas de transsition, on reaffiche la navigation.
+        setTimeout(() => {
+          generatedElement.overlay.classList.remove("loading-file");
+        }, 1100);
         resolv(img);
       });
     });
@@ -250,12 +292,12 @@ class GalleryOverlay {
     generatedElement.overlay.className = "gallery-overlay-section-overlay";
     generatedElement.imagesContainer.classList.add("gallery-overlay-images-container", "d-flex", "justify-content-center", "align-items-center");
     generatedElement.image.classList.add("gallery-overlay-section-current-image", "img-responsive", "img-fluid");
-    generatedElement.prevButton.className = "gallery-overlay-section-btn-prev";
+    generatedElement.prevButton.className = "gallery-overlay-navigation btn-prev d-flex align-items-center";
     generatedElement.prevButton.innerHTML =
-      '<svg fill="#fff" width="150" height="150" viewBox="-1.5 -0.938 4.5 4.5" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-chevron-right" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" transform="matrix(-1,0,0,-1,0,0)"><path d="M.996 1.326.068.398A.188.188 0 0 1 .333.133l1.061 1.061a.19.19 0 0 1 0 .265L.333 2.52a.188.188 0 0 1-.265-.265z"></path></svg>';
-    generatedElement.nextButton.className = "gallery-overlay-section-btn-next";
+      '<svg fill="#fff" width="80" height="80" viewBox="-1.5 -0.938 4.5 4.5" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-chevron-right" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" transform="matrix(-1,0,0,-1,0,0)"><path d="M.996 1.326.068.398A.188.188 0 0 1 .333.133l1.061 1.061a.19.19 0 0 1 0 .265L.333 2.52a.188.188 0 0 1-.265-.265z"></path></svg>';
+    generatedElement.nextButton.className = "gallery-overlay-navigation btn-next d-flex align-items-center justify-content-end";
     generatedElement.nextButton.innerHTML =
-      '<svg fill="#fff" width="150" height="150" viewBox="-1.5 -0.938 4.5 4.5" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-chevron-right"><path d="M.996 1.326.068.398A.188.188 0 0 1 .333.133l1.061 1.061a.19.19 0 0 1 0 .265L.333 2.52a.188.188 0 0 1-.265-.265z"/></svg>';
+      '<svg fill="#fff" width="80" height="80" viewBox="-1.5 -0.938 4.5 4.5" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-chevron-right"><path d="M.996 1.326.068.398A.188.188 0 0 1 .333.133l1.061 1.061a.19.19 0 0 1 0 .265L.333 2.52a.188.188 0 0 1-.265-.265z"/></svg>';
     generatedElement.exitButton.className = "gallery-overlay-section-btn-exit";
     generatedElement.exitButton.innerHTML =
       '<svg viewBox="0 0 3 3" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path fill-opacity=".01" d="M0 0h3v3H0z"/><path d="m.5.5 2 2m-2 0 2-2" stroke="#fff" stroke-width=".25" stroke-linecap="round" stroke-linejoin="round"/></svg>';
