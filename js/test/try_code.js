1d019ae963ae8e2d6c70923a4e950b93e287fabb
kouwa stephane <kksasteph888@gmail.com>
2025-01-27 19:19:08 +0100

    Maj

--- js/test/try_code.js
+++ js/test/try_code.js
@@ -3,20 +3,20 @@
  * on peut ajouter "import "@stephane888/wbu-atomique/js/test/try_code.js;"
  */
 // Code for orthers things.
-
-//
-// Drupal.behaviors.customScroll = {
-//     attach: function (context, settings) {
-//         if (document.querySelector('.custom-paragraph-scroll')) {
-//             const elements = once("not-front-page", '.custom-paragraph-scroll', context)
-//             console.log("elements = ", elements);
-//             window.once('not-front-page').on('scroll', function () {
-//                 if (window.pageYOffset > 50) { // Ajustez la valeur selon vos besoins
-//                 document.body.classList.add('scrolled');
-//                 } else {
-//                 document.body.classList.remove('scrolled');
-//                 }
-//                 });
-//         }
-//     }
-// }
+Drupal.behaviors.hbk_d45d_navigate_to_next = {
+  attach: function (context, settings) {
+    if (context.querySelectorAll && context.querySelectorAll('[data-action="navigate-next"]')) {
+      once("paragraph_navigate_to_next", '[data-action="navigate-next"]', context).forEach((scrollToTopBtn) => {
+        scrollToTopBtn.addEventListener("click", function () {
+          scrollToTopBtn.classList.add("is-active");
+          // Obtenir la position du bouton par rapport au haut de la page
+          const buttonPosition = scrollToTopBtn.getBoundingClientRect().top + window.scrollY;
+          window.scrollTo({
+            top: buttonPosition,
+            behavior: "smooth", // Pour un défilement fluide
+          });
+        });
+      });
+    }
+  },
+};
